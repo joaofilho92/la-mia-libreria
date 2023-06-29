04044b0da6d65e29f0dc4429ccdc31f5ce5e91bb
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-06-29 16:35:08 +0200

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className="flex h-20 bg-fuchsia-700 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+      <span className="text-fuchsia-200">Ciao, Giulia</span>
+      <h1 className="text-fuchsia-200 text-xl">La Mia Libreria</h1>
+    </div>
+  );
+};
+
+export default Header

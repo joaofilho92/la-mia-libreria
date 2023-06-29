ec82013d5fb84a2f4ee55e088146a555fb1d9bca
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-06-29 16:59:08 +0200

    json-server install

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div className="absolute inset-x-0 bottom-0 h-16 text-center text-fuchsia-200">
+      <h1>Copyright © 2023 Joao Batista Lopes Filho Dev.</h1>
+    </div>
+  );
+}
+
+export default Footer
